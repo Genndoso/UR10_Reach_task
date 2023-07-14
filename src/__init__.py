4920e3d3f181dd9dc88d162fef9fdd5d6a02c6bd
Максимилиан <pavlovmaksimilian1@gmail.com>
2023-07-14 17:42:25 +0300

    Make module

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .envs import *
+from .objects import *
+from .visualize import *
+from .agents import *
---
+++ src/agents/__init__.py
@@ -0,0 +1,2 @@
+from .stable_baselines import stable_bs_agent
+from .DDPG_torch import *
